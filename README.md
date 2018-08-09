709726f4b94c426dd113ff4b2ec8cd6407e24544
NG <nuwan_g@live.com>
2018-08-09 13:03:09 +0530

    Add icons, price and order by rank

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cryptocurreny Application
 Built using React
+
+# API Used
 Api - Coinmarketcap.com
--- src/App.js
+++ src/App.js
@@ -9,26 +9,64 @@ class App extends Component {
     super(props);
 
     this.state = {
-      cryptos: []
+      cryptos: [],
+      map1: []
     };
   }
   
   componentDidMount() {
-    axios.get(' https://api.coinmarketcap.com/v2/ticker/?limit=25')
+    axios.get(' https://api.coinmarketcap.com/v2/ticker/?limit=100')
       .then(res => {
         const cryptos = res.data.data;
-        console.log(cryptos);
-         this.setState({cryptos});
-      })      
-  }
+        const map1 = Object.values(cryptos).map((val) => {
+          return {
+            id: val.id,           
+            rank: val.rank,
+            name: val.name,
+            symbol: val.symbol,
+            tot: val.total_supply,
+            circ: val.circulating_supply
+          }
+        });
+      axios.get('https://s2.coinmarketcap.com/static/img/coins/32x32/1808.png')
+      .then(res => {
+        const bigData = res.data;
+        console.log(bigData);
+      })
+      
+        function compare(a,b) {
+          if (a.rank < b.rank)
+            return -1;
+          if (a.rank > b.rank)
+            return 1;
+          return 0;
+        };   
+        map1.sort(compare);
+        this.setState({cryptos});
+        this.setState({map1});
+    });      
+  };
+  // 'https://s2.coinmarketcap.com/static/img/coins/32x32/1808.png'
   render() {
     return (
       <div className="App">
-       {Object.values(this.state.cryptos).map((value) => (
+      <h1 className="title">Top 100 Cryptocurrencies By Market Capitalization</h1>
+      <div className="crypto-container">
+      <span className="left">Name </span>
+      <span className="left">Market Cap </span>
+      <span className="left">Price </span>
+      <span className="left">Price </span>
+      <span className="left">Price </span>
+      <span className="right">Price</span>
+
+      </div>
+       {Object.values(this.state.map1).map((value) => (
          <div className="crypto-container" key={value.id}>
-          <span id={value.id} className="left" key={value.name}>{value.name}</span>
+         <img src={`https://s2.coinmarketcap.com/static/img/coins/32x32/${value.id}.png`} alt="Smiley face" height="42" width="42" />
+         <span id={value.id} className="left" key={value.rank}>{value.rank}. </span>
+          <span id={value.id} className="left" key={value.name}>{value.name} ({value.symbol})</span>
           {Object.values(this.state.cryptos[value.id].quotes).map((last) => (
-           <span className="right" key={last.price}>{numeral(last.price).format('$0,0.0000')}</span>
+           <span className="right" key={last.price}>{numeral(last.price).format('$0,0.00')}</span>
           ))}
          </div>
        ))}
